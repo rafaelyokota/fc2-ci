89d165862825a388e9cc4c48ad92b78a076d5701
rafael yokota <rafael.yokota.jp@gmail.com>
2021-07-24 08:02:16 -0300

    asdhs

--- math_test.go
+++ math_test.go
@@ -1,7 +1,7 @@
 package testing
 
 func TestSoma(t *testing.T){
-	total := Soma(15,15)
+	total := soma(15,15)
 	esperado := 30
 	if total != esperado{
 		t.Errorf("Resultado soma invalido: Resultado %d", esperado)
